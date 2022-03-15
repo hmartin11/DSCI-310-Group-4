a15481f6a2ff788b4450f15badf59736d7a50b66
YuYT98 <yuyt1998@gmail.com>
2022-03-14 20:28:57 -0700

    wrote function documentation for preprocess function

---
+++ src/preprocess.py
@@ -0,0 +1,16 @@
+#' drop all rows containing null values then split the df into training and testing part
+#' Creates a new data frame with with the same columns as input and 4 rows,
+#'
+#' @param dataframe The shuffled data frame
+#' @param train_frac The percent that ends up in the training set (range 0-1).
+#'
+#' @return training and testing dataframes
+#'
+#' @export
+#'
+#' @examples
+#' preprocess(dataframe, 0.8)
+
+def preprocess(df, train_frac):
+    return
+
