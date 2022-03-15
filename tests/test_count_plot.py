2652ac745eba70e56b0588ce8445e7b10bfc55eb
Shravan37 <shravan.chaniara@gmail.com>
2022-03-15 11:59:15 -0700

    Fixed tests and tested code coverage

--- tests/test_count_plot.py
+++ tests/test_count_plot.py
@@ -1,27 +1,25 @@
+from src import count_plot
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
-from src import function_count_plot
+import pytest
 
 
-data = pd.DataFrame(np.linspace(4,7,10), columns=["x1"])
-data1 = pd.DataFrame(np.linspace(7,4,14), columns=["x1"])
+
+data = pd.DataFrame({'PAY_0': [-2, 1, 0, 0, -1], 'default_payment': [1, 1, 0, 1, 0]})
 labels = ["0","1"]
-plot = count_plot(data, x, y, "Count Plot")
-fig, ax = plt.subplots()
+plot = count_plot(data, data["PAY_0"], data["default_payment"], "Count Plot")
+fig, ax = sns.countplot()
 
 def test_return_type():
-    """
-    Test for the correct return type of the function is an Axes object
-    """
+    """Test for the correct return type of the function is an axes object"""
     assert type(plot) == type(ax)
 
 def test_readability():
-    """
-    Test for the correct label for X,y axis, legend and title
-    """
+    """Test for the correct label for X,y axis, legend and title"""
     assert plot.get_xlabel() == "PAY_0"
     assert plot.get_ylabel() == "Count"
     assert plot.get_legend().get_texts()[0].get_text() == labels[0]
     assert plot.get_legend().get_texts()[1].get_text() == labels[1]
     assert plot.get_title() == "Count Plot"
+
