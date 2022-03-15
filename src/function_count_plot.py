8f43945a9133eed7940893c5e5a2bcb226d419fd
Shravan37 <shravan.chaniara@gmail.com>
2022-03-14 19:13:14 -0700

    Wrote function documentation for countplot function
    Accidentally created a python notebook instead of a file.

---
+++ src/function_count_plot.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# ' Plot a Countplots
+# '
+# 'Creates countplots for data frame with all the given x and y values, 
+# '
+# ' @param A data frame with all the values of which we want the countplots 
+# ' @param array a list of numbers which represents the different payments for x-values
+# ' @param array a list of numbers which represents the default payment for y-values
+# ' @param string the name of the plot
+# '
+# ' @return An plot of the counts between two parameters of the given data frame.
+# '         The plot should have a x label, a y label and a title.
+# '
+# ' @export
+# '
+# ' @examples
+# ' count_plot(train_df, x, y, "Count plot")
+
+def count_plot(data, x, y, name):
+    
+    return 
