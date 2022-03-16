603b02c7336a9d80ce3cca584679139af30dbb41
hmartin11 <hmartin11@telus.net>
2022-03-15 20:04:48 -0700

    reformatted functions using black

--- src/function_count_plot.py
+++ src/function_count_plot.py
@@ -3,9 +3,9 @@ import seaborn as sns
 
 # ' Plot a Countplots
 # '
-# 'Creates countplots for data frame with all the given x and y values, 
+# 'Creates countplots for data frame with all the given x and y values,
 # '
-# ' @param A data frame with all the values of which we want the countplots 
+# ' @param A data frame with all the values of which we want the countplots
 # ' @param array a list of numbers which represents the different payments for x-values
 # ' @param string the name of the plot
 # '
@@ -17,8 +17,9 @@ import seaborn as sns
 # ' @examples
 # ' count_plot(train_df, x, "Count plot")
 
+
 def count_plot(data, x, name):
-    fig = sns.countplot(data = data, hue="default_payment", x = x)
+    fig = sns.countplot(data=data, hue="default_payment", x=x)
     plt.xlabel(str(x))
     plt.ylabel("count")
     plt.title(name)
--- src/functions.py
+++ src/functions.py
@@ -1,2 +1,2 @@
 def my_add(x, y):
-    return(x + y)
+    return x + y
