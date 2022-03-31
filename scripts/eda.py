2febc0752fb691d37d8f34918c3f79bbf5700040
hmartin11 <hmartin11@telus.net>
2022-03-30 18:53:43 -0700

    updated eda script

--- scripts/eda.py
+++ scripts/eda.py
@@ -1,7 +1,7 @@
 import os
 import sys
 sys.path.append('.')
-from src import count_plot as cp
+from src.function_count_plot import count_plot as cp
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -12,13 +12,16 @@ def make_heatmap(input_path, output_path):
     train_df = pd.read_csv(input_path,sep = ",")
     plt.figure(figsize=(25,25))
     sns.heatmap(train_df.corr(),cbar=True,annot=True,cmap='Reds')
-    plt.savefig('data/plots.png')
+    plt.savefig(output_path)
 
 
 
 def main():
 
-    input_path = data/processed_df
+    input_path = "data/processed_train_data.csv"
+    output_path = 'data/heatmap.png'
+
+    make_heatmap(input_path, output_path)
 
 if __name__ == "__main__":
     main()
