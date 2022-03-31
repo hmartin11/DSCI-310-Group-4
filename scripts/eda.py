38620f22ee7541c542660430c25ea0695d00c374
hmartin11 <hmartin11@telus.net>
2022-03-30 17:36:00 -0700

    started script3 eda

---
+++ scripts/eda.py
@@ -0,0 +1,14 @@
+import os
+import sys
+sys.path.append('.')
+from src import count_plot as cp
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def make_heatmap(input_path, output_path):
+
+    df = pd.read_csv(input_path,sep = ",")
+    plt.figure(figsize=(25,25))
+    sns.heatmap(train_df.corr(),cbar=True,annot=True,cmap='Reds')
