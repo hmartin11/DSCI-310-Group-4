795d8661a6b9850f1b8aabdc33025e3fe8ed2b3b
hmartin11 <hmartin11@telus.net>
2022-04-01 10:26:02 -0700

    fixed script1

--- scripts/read_data_from_url.py
+++ scripts/read_data_from_url.py
@@ -5,16 +5,22 @@
 # load libraries/packages or source functions from other scripts
 import pandas as pd
 
+import ssl
+
+
 
 # load the dataset
 def read_data_from_url(url, filepath):
+    ssl._create_default_https_context = ssl._create_unverified_context
     # load the dataset
     df = pd.read_excel(url, skiprows=1)
     # write dataframe to filepath
     df.to_csv(filepath, index = False)
 
 def main():
-    ""
+
+    
+    
     url = "https://archive.ics.uci.edu/ml/machine-learning-databases/00350/default%20of%20credit%20card%20clients.xls"
     path = 'data/card_default_data.csv'   
     read_data_from_url(url, path)
