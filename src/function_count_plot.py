04c68426c6ab0af2d0d58d853ca6f8ed4b33b5ba
Shravan37 <shravan.chaniara@gmail.com>
2022-04-08 19:34:18 -0700

    Edited and fixed countplot function and tests

--- src/function_count_plot.py
+++ src/function_count_plot.py
@@ -9,7 +9,7 @@ import seaborn as sns
 # ' @param array a list of numbers which represents the different payments for x-values
 # ' @param string the name of the plot
 # '
-# ' @return An plot of the counts between two parameters of the given data frame.
+# ' @return A plot of the counts between two parameters of the given dataframe.
 # '         The plot should have a x label, a y label and a title.
 # '
 # ' @export
@@ -18,8 +18,29 @@ import seaborn as sns
 # ' count_plot(train_df, x, "Count plot")
 
 
-def count_plot(data, x, name):
-    fig = sns.countplot(data=data, hue="default_payment", x=x)
+def count_plot(df, x, name):
+    # returns a figure of a counts between two parameters of the given dataframe,
+    # and has a given title, and correct x and y labels.
+
+    try:
+        if not isinstance(df, pd.DataFrame):
+            raise AttributeError("Invalid input: df is not a DataFrame")
+            
+        if not isinstance(name, str):
+            raise AttributeError("Invalid input: name is not a String")
+        
+        if not isinstance(x, str):
+            raise AttributeError("Invalid input: x is not a String")
+            
+    except Exception as err:
+        print("Something has gone wrong", err)
+        return err
+
+    if df.empty:
+        print("DataFrame is empty!")
+        return df
+    
+    fig = sns.countplot(data=df, hue="default_payment", x=x)
     plt.xlabel(str(x))
     plt.ylabel("count")
     plt.title(name)
